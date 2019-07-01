47e9d409fdb1b82d9a14e21701fce20faf7981bb
Isael <michael.puelle.ordenes@gmail.com>
2019-06-30 20:11:51 -0400

    modals

--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Modal} from "react-bootstrap";
+import { Modal} from "react-bootstrap";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 
